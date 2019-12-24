332401f29bac6fd6cd6efeba48f5c0fbb6edbf1b
David Meybohm <dmeybohm@gmail.com>
2019-12-24 15:18:18 -0500

    Add files

--- src/AnalyzerDiff.php
+++ src/AnalyzerDiff.php
@@ -1,11 +1,11 @@
-<?php declare(strict_types=1);
+<?php declare(strict_types = 1);
 
 namespace ProfoundInventions\Nitpick;
 
 interface AnalyzerDiff
 {
     /**
-     * @return Error
+     * @return Error[]
      */
     public function errors(): array;
 }
